d6c718ff955fcff54de77ff0586d736da5771526
Саня <sanya@Sergejs-MacBook-Air.local>
2020-09-24 15:37:28 +0600

    Добавил дефолтные значения

--- Models/Laptop.cs
+++ Models/Laptop.cs
@@ -1,9 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace OnlineShop.Models
 {
     public class Laptop : Entity
     {
+        [Column("Название")]
         public string Name { get; set; }
+        [Column("Модель")]
         public string Model { get; set; }
+        [Column("Производитель")]
         public string Manufacturer { get; set; }
         //public string CPU { get; set; }
         //public string RAM { get; set; }
--- Models/Profile.cs
+++ Models/Profile.cs
@@ -1,8 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace OnlineShop.Models
 {
     public class Profile
     {
+        [Column("Имя")]
         public string Name { get; set; }
+        [Column("Возраст")]
         public int Age { get; set; }
         public User User { get; set; }
     }
